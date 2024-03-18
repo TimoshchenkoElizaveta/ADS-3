2f90f4af27e71923702bd38d47ca23274f46d6f6
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-03-18 19:38:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,6 @@ int cbinsearch(int *arr, int size, int value) {
     int count = 0;
     int left = 0;
     int right = size - 1;
-
     while (left <= right) {
         int mid = left + (right - left) / 2;
         if (arr[mid] == value) {
@@ -14,12 +13,12 @@ int cbinsearch(int *arr, int size, int value) {
                 count++;
                 i--;
             }
-                i = mid + 1;
+            i = mid + 1;
             while (i < size && arr[i] == value) {
                 count++;
                 i++;
             }
-            break;
+            return count;
         } else if (arr[mid] < value) {
             left = mid + 1;
         } else {
@@ -28,12 +27,12 @@ int cbinsearch(int *arr, int size, int value) {
     }
     return count;
 }
-
 int main() {
-    int arr[] = {1, 2, 2, 2, 3, 4, 5};
+    int arr[] = {1, 2, 2, 3, 4, 5, 5, 5, 6, 7};
     int size = sizeof(arr) / sizeof(arr[0]);
-    int value = 2;
+    int value = 5;
+    
     int result = cbinsearch(arr, size, value);
-    std::cout << "Number of occurrences of " << value << " in the array: " << result << std::endl;
+    printf("%dn", result);
   return 0; // если ничего не найдено
 }
